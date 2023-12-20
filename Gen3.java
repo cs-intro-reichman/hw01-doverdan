720cdeb250a4767c23c330e1b55185fcaea5f227
Dan Dover <45949873+doverdan@users.noreply.github.com>
2023-12-20 09:31:09 +0200

    Gen3 fix to random

--- Gen3.java
+++ Gen3.java
@@ -6,20 +6,21 @@ import java.util.Random;
 
 public class Gen3{
 	public static void main(String[] args) {
-		Random rand = new Random();
 
 		//Receiving the limits 
 		int a = Integer.parseInt(args[0]);
-		int b = Integer.parseInt(args[1]);  
+		int b = Integer.parseInt(args[1]); 
+		int minrange = Math.min(a,b);
+		int maxrange = Math.max(a,b); 
 
 		//Generating 3 randoms
-		int rand1 = rand.nextInt(a,b);
-		int rand2 = rand.nextInt(a,b);
-		int rand3 = rand.nextInt(a,b);
+		int rand1 = (int)(Math.random()*(maxrange-minrange))+minrange;
+		int rand2 = (int)(Math.random()*(maxrange-minrange))+minrange;
+		int rand3 = (int)(Math.random()*(maxrange-minrange))+minrange;
 
 		//Checking for the minimal value generated
 		int check_ans = Math.min(rand1,rand2);
-		int final_ans = Math.min(check_ans,rand3);
+		check_ans = Math.min(check_ans,rand3);
 
 		//Printing the generated numbers
 		System.out.println(rand1);
@@ -27,6 +28,6 @@ public class Gen3{
 		System.out.println(rand3);
 
 		//Printing the answer
-		System.out.println("The minimal generated number was " +final_ans);
+		System.out.println("The minimal generated number was " +check_ans);
 	}
 }
